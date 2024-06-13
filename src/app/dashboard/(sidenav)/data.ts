54602966cc28d6a1825d8fae0e4d2ae0c6394b33
Kristijan Korać <korac.kristijan@gmail.com>
2024-06-13 21:06:33 +0200

    feat: add new dashboard pages
    - Adds the rest of the pages.
    - Utilizes new ui components.

--- src/app/dashboard/(sidenav)/data.ts
+++ src/app/dashboard/(sidenav)/data.ts
@@ -17,10 +17,10 @@ export const links: NavLink[] = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   { name: 'Trucks Capacity', href: '/dashboard/capacity', icon: TruckIcon },
   { name: 'Shipments', href: '/dashboard/shipments', icon: CubeTransparentIcon },
-  { name: 'Warehouse', href: '/dashboard/warehouse', icon: HomeModernIcon },
+  { name: 'Warehouse', href: '/dashboard/warehouses', icon: HomeModernIcon },
   // { name: 'Loading Planer', href: '/loading-planer', icon: HomeIcon }, // TODO
   { name: 'Fleet', href: '/dashboard/fleet', icon: TruckIcon },
-  { name: 'Assign Trucks', href: '/assign/dashboard-trucks', icon: SquaresPlusIcon },
+  { name: 'Assign Trucks', href: '/dashboard/assign-trucks', icon: SquaresPlusIcon },
   { name: 'Employees', href: '/dashboard/employees', icon: UserGroupIcon },
   { name: 'Analytics', href: '/dashboard/analytics', icon: PresentationChartLineIcon },
 ];
--- src/app/dashboard/analytics/page.tsx
+++ src/app/dashboard/analytics/page.tsx
@@ -3,7 +3,7 @@ import { Box, Text } from '@/ui';
 export default function Page() {
   return (
     <Box>
-      <Text as="h1" className="text-xl font-bold">Dashboard</Text>
+      <Text as="h1" className="text-xl font-bold">Analytics</Text>
     </Box>
   );
 }
