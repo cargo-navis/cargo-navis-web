1847da3b731c6e4fbd573ac7568d71dd35370131
Kristijan Korać <korac.kristijan@gmail.com>
2024-06-17 21:41:35 +0200

    wip(employees): add initial page loader

--- src/app/dashboard/employees/page.tsx
+++ src/app/dashboard/employees/page.tsx
@@ -1,9 +1,17 @@
 import { Box, Heading } from '@/ui';
+import { getEmployees } from '@/api/employees';
 
-export default function Page() {
+export default async function Page() {
+  const employees = await getEmployees();
+
   return (
     <Box>
       <Heading as="h1" variant="text-xl">Employees</Heading>
+      <Box as="ul" className="list-decimal">
+        {employees.map(e => {
+          return <Box as="li" key={e.id}>{`${e.firstName} ${e.lastName}`}</Box>
+        })}
+      </Box>
     </Box>
   );
 }
