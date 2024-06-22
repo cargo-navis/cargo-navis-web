52525cace895b222d431f0fbb457ef71e25f84a0
Kristijan Korać <korac.kristijan@gmail.com>
2024-06-22 18:59:58 +0200

    fix: add overflow to main content

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <Box className="translate-x-[-16rem] flex flex-col absolute h-screen w-64  py-6 px-4 md:static md:h-auto md:w-64 md:translate-x-0 bg-sidebar-gradient transition ease-in-out">
         <Sidebar />
       </Box>
-      <Box as="main" className="flex-grow p-6 md:p-12">
+      <Box as="main" className="flex-grow p-6 md:p-12 overflow-y-auto">
         {children}
       </Box>
     </Box>
