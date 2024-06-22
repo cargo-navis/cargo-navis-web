02ddcc9639bd66d44a1750a113529fbdee3740b6
Kristijan Korać <korac.kristijan@gmail.com>
2024-06-22 19:00:42 +0200

    feat: add EmployeesTable component
    - Renders initial version of employees table

---
+++ src/app/dashboard/employees/EmployeesTable.tsx
@@ -0,0 +1,100 @@
+'use client';
+
+import { Box, Table, Text } from '@/ui';
+import clsx from 'clsx';
+import { CheckIcon, PencilIcon, TrashIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { createColumnHelper } from '@tanstack/react-table';
+import { Employee } from '@/lib/employees';
+import { OccupationPill } from '@/app/dashboard/employees/OccupationPill';
+
+const columnHelper = createColumnHelper<Employee>();
+
+export const columns = [
+  columnHelper.display({
+    id: 'avatar',
+    cell: props => {
+      const memberName: string = props.row.getValue('fullName');
+      const [fName, lName] = memberName.split(' ');
+
+      return (
+        <Box className="py-3 pl-3">
+          <Box className="flex items-center justify-center w-[50px] h-[50px] rounded-circle bg-teal-900">
+            <Text className="text-light-50">{fName[0]+lName[0]}</Text>
+          </Box>
+        </Box>
+      );
+    }
+  }),
+  columnHelper.accessor(row => `${row.firstName} ${row.lastName}`, {
+    id: 'fullName',
+    header: 'Name',
+    cell: props => {
+      const name = props.getValue();
+      const position = props.row.original.position;
+
+      return (
+        <Box className="py-3">
+          <Box className="flex flex-col gap-1">
+            <Text color="text-color-1" variant="text-m-bold">{name}</Text>
+            <OccupationPill occupation={position} text={position} />
+          </Box>
+        </Box>
+      );
+    }
+  }),
+  columnHelper.accessor('email', {
+    header: 'Email',
+    cell: props => {
+      const email = props.getValue();
+
+      return (
+        <Box className="flex flex-col gap-1">
+          <Text color="text-color-2" variant="text-s">{email}</Text>
+        </Box>
+      )
+    },
+  }),
+  columnHelper.accessor('driverLicenceCategories', {
+    header: 'Categories',
+    cell: props => {
+      const licenceCategories = props.getValue();
+
+      if(!licenceCategories) return null;
+
+      return (
+        <Box className="flex gap-1 align-middle">
+          {licenceCategories.map(v => <span className="bg-light-700 px-1" key={v}>{v}</span>)}
+        </Box>
+      );
+    },
+  }),
+  columnHelper.accessor('adr', {
+    cell: props => {
+      const adr = props.getValue();
+
+      const color = adr ? 'text-teal-400' : 'text-teal-900'
+
+      return (
+        <Box className={clsx(color, 'h-[32px] w-[32px]')}>{props.getValue() ? <CheckIcon /> : <XMarkIcon />}</Box>
+      );
+    },
+    header: 'ADR',
+  }),
+  columnHelper.display({
+    id: 'actions',
+    cell: () => {
+      return (
+        <Box className="flex align-middle gap-3 w-[140px] h-[32px] text-teal-400">
+          <PencilIcon className="w-6" />
+          <TrashIcon className="w-6" />
+        </Box>
+      )
+    }
+  })
+];
+
+export function EmployeesTable({ employees }: { employees?: Employee[] }) {
+  return (
+    <Table data={employees} columns={columns} />
+  );
+}
--- src/app/dashboard/employees/page.tsx
+++ src/app/dashboard/employees/page.tsx
@@ -1,4 +1,6 @@
 import { Box, Heading } from '@/ui';
+
+import { EmployeesTable } from './EmployeesTable';
 import { getEmployees } from '@/api/employees';
 
 export default async function Page() {
@@ -7,10 +9,8 @@ export default async function Page() {
   return (
     <Box>
       <Heading as="h1" variant="text-xl">Employees</Heading>
-      <Box as="ul" className="list-decimal">
-        {employees.map(e => {
-          return <Box as="li" key={e.id}>{`${e.firstName} ${e.lastName}`}</Box>
-        })}
+      <Box className="py-5">
+        <EmployeesTable employees={employees} />
       </Box>
     </Box>
   );
