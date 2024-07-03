62fd38ea78a9784f15cf304227d614d2348bee1d
Kristijan Korać <korac.kristijan@gmail.com>
2024-07-03 19:40:13 +0200

    feat(EmployeesTable): add link to details page

--- src/app/dashboard/employees/EmployeesTable.tsx
+++ src/app/dashboard/employees/EmployeesTable.tsx
@@ -1,8 +1,10 @@
 'use client';
+
 import clsx from 'clsx';
-import { CheckCircleIcon, PencilIcon, TrashIcon, XCircleIcon, DocumentDuplicateIcon } from '@heroicons/react/24/outline';
+import { CheckCircleIcon, XCircleIcon, DocumentDuplicateIcon } from '@heroicons/react/24/outline';
 import { createColumnHelper } from '@tanstack/react-table';
 import { useMemo } from 'react';
+import Link from 'next/link';
 
 import { CategoryLabel } from '@/app/dashboard/employees/CategoryLabel';
 import { Box, Table, Text } from '@/ui';
@@ -24,13 +26,16 @@ export function EmployeesTable({ employees }: { employees?: Employee[] }) {
         cell: props => {
           const memberName: string = props.row.getValue('fullName');
           const [fName, lName] = memberName.split(' ');
+          const id = props.row.original.id;
 
           return (
-            <Box className="py-3 pl-3">
-              <Box className="flex items-center justify-center w-[50px] h-[50px] rounded-circle bg-teal-900">
-                <Text className="text-light-50">{fName[0]+lName[0]}</Text>
+            <Link href={`/dashboard/employees/${id}`}>
+              <Box className="py-3 pl-3">
+                <Box className="flex items-center justify-center w-[50px] h-[50px] rounded-circle bg-teal-900">
+                  <Text className="text-light-50">{fName[0]+lName[0]}</Text>
+                </Box>
               </Box>
-            </Box>
+            </Link>
           );
         }
       }),
@@ -40,15 +45,17 @@ export function EmployeesTable({ employees }: { employees?: Employee[] }) {
         size: 275,
         cell: props => {
           const name = props.getValue();
-          const position = props.row.original.position;
+          const { position, id } = props.row.original;
 
           return (
-            <Box className="py-3">
-              <Box className="flex flex-col gap-1">
-                <Text color="text-color-1" variant="text-m-bold">{name}</Text>
-                <OccupationPill occupation={position} text={position} />
+            <Link href={`/dashboard/employees/${id}`}>
+              <Box className="py-3">
+                <Box className="flex flex-col gap-1">
+                  <Text color="text-color-1" variant="text-m-bold">{name}</Text>
+                  <OccupationPill occupation={position} text={position} />
+                </Box>
               </Box>
-            </Box>
+            </Link>
           );
         }
       }),
@@ -109,18 +116,6 @@ export function EmployeesTable({ employees }: { employees?: Employee[] }) {
         },
         header: 'ADR',
       }),
-      columnHelper.display({
-        size: 100,
-        id: 'actions',
-        cell: () => {
-          return (
-            <Box className="invisible group-hover/row:visible flex justify-center gap-4 w-full h-[32px] text-dark-500 dark:text-light-300">
-              <PencilIcon className="w-5 cursor-pointer" />
-              <TrashIcon className="w-5 cursor-pointer" />
-            </Box>
-          )
-        }
-      })
     ];
   }, [copyCellValue]);
 
