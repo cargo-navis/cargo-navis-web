d481d2774c5b7009f78a9d5da5ad1120af82348c
Kristijan Korać <korac.kristijan@gmail.com>
2024-07-16 22:17:04 +0200

    feat: upgrade NewEmployeeForm to real form component
    - Utilize existing Form components.

--- src/app/dashboard/employees/new/NewEmployeeForm.tsx
+++ src/app/dashboard/employees/new/NewEmployeeForm.tsx
@@ -1,11 +1,11 @@
 'use client';
 
 import 'dayjs/locale/hr';
-import { useState } from 'react';
 import '@mantine/dates/styles.css';
 
-import { Box, Datepicker } from '@/ui';
-import { CheckboxGroupWithLabels, DatepickerWithLabels, RadioGroupWithLabels, TextInputWithLabels } from '@/ui/hocs';
+import { Box } from '@/ui';
+import { FormProvider, useForm } from 'react-hook-form';
+import { FormCheckboxGroup, FormDatepicker, FormRadioGroup, FormTextInput } from '@/lib/components/form';
 
 const positionOptions = [
   { label: 'Manager', value: 'manager' },
@@ -26,64 +26,74 @@ const categoryOptions = [
   { label: 'C2', value: 'c2' },
 ];
 
+const dummyDefaultValues = {
+  "first_name": "Kristijan",
+  "last_name": "Korac",
+  "phone_number": "098 192 8375",
+  "email": "korac",
+  "gov_id": "HR123456789",
+  "position": "driver",
+  "categories": [
+    "b2",
+    "b"
+  ],
+  "adr": "adr_false",
+  "expiry_date": "2024-07-24"
+};
+
 export const NewEmployeeForm = () => {
-  const [name, setName] = useState('');
-  const [lName, setLname] = useState('');
-  const [phone, setPhone] = useState('');
-  const [email, setEmail] = useState('');
-  const [govId, setGovId] = useState('');
+  const formMethods = useForm({
+    defaultValues: dummyDefaultValues
+  });
 
-  const [position, setPosition] = useState('');
-  const [adr, setAdr] = useState(adrOptions[0].value);
-  const [categories, setCategories] = useState<string[]>([]);
+  const { watch, handleSubmit } = formMethods;
+  const values = watch();
 
-  const [date, setDate] = useState<string | null>(null);
+  function handleFormSubmit(data: any) {
+    console.log('form submitted');
+    console.log(data);
+  }
 
   return (
-    <Box className="flex flex-col gap-10">
-      <Box className="flex flex-col gap-4 w-[500px]">
-        <Box className="flex gap-4">
-          <Box className="flex-grow">
-            <TextInputWithLabels label="First Name" value={name} onChange={setName} />
-          </Box>
-          <Box className="flex-grow">
-            <TextInputWithLabels label="Last Name" value={lName} onChange={setLname} />
-          </Box>
-        </Box>
-        <Box>
-          <TextInputWithLabels label="Phone number" type="tel" value={phone} onChange={setPhone} />
-        </Box>
-        <Box>
-          <TextInputWithLabels label="Email" type="email" value={email} onChange={setEmail} />
-        </Box>
-        <Box>
-          <TextInputWithLabels label="Government ID" value={govId} onChange={setGovId} />
-        </Box>
-        <RadioGroupWithLabels label="Position" options={positionOptions} value={position} onChange={setPosition} />
-        {position === 'driver' && (
-          <Box className="flex flex-col gap-4">
-            <Box className="flex gap-3">
-              <Box className="flex-1">
-                <CheckboxGroupWithLabels label="Categories" options={categoryOptions} values={categories} onChange={setCategories} />
-              </Box>
-              <Box className="flex-1">
-                <RadioGroupWithLabels label="ADR" options={adrOptions} value={adr} onChange={setAdr} />
-              </Box>
+    <FormProvider {...formMethods}>
+      <Box as="form" className="flex flex-col gap-10" onSubmit={handleSubmit(handleFormSubmit)}>
+        <Box className="flex flex-col gap-4 w-[500px]">
+          <Box className="flex gap-4">
+            <Box className="flex-grow">
+              <FormTextInput name="first_name" label="First Name" />
             </Box>
-            <DatepickerWithLabels label="Date of Expiry" value={date} onChange={setDate} />
+            <Box className="flex-grow">
+              <FormTextInput name="last_name" label="Last Name" />
+            </Box>
           </Box>
-        )}
+          <Box>
+            <FormTextInput name="phone_number" label="Phone Number" type="tel" />
+          </Box>
+          <Box>
+            <FormTextInput name="email" label="Email" type="email" />
+          </Box>
+          <Box>
+            <FormTextInput name="gov_id" label="Government ID" />
+          </Box>
+          <FormRadioGroup name="position" label="Position" options={positionOptions} />
+          {values?.position === 'driver' && (
+            <Box className="flex flex-col gap-4">
+              <Box className="flex gap-3">
+                <Box className="flex-1">
+                  <FormCheckboxGroup name="categories" label="Categories" options={categoryOptions} />
+                </Box>
+                <Box className="flex-1">
+                  <FormRadioGroup name="adr" label="ADR" options={adrOptions} />
+                </Box>
+              </Box>
+              <FormDatepicker name="expiry_date" label="Expiry Date" />
+            </Box>
+          )}
+        </Box>
+        <Box as="pre" className="absolute right-[600px]">
+          {JSON.stringify(values, null, 2)}
+        </Box>
       </Box>
-      <Box as="pre" className="absolute right-[1000px]">
-        <Box>Name: {name}</Box>
-        <Box>Last Name: {lName}</Box>
-        <Box>Phone: {phone}</Box>
-        <Box>Email: {email}</Box>
-        <Box>Position: {position}</Box>
-        <Box>Categories: {categories.map(c => `${c} `)}</Box>
-        <Box>ADR: {adr}</Box>
-        <Box>Date of expiration: {date}</Box>
-      </Box>
-    </Box>
+    </FormProvider>
   );
 }
