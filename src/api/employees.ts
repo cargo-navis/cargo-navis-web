694ac96ff14bf5ebca099797ba6e2cced4a1eccd
Kristijan Korać <korac.kristijan@gmail.com>
2024-07-17 21:14:12 +0200

    wip: add dummy employee API fns

--- src/api/employees.ts
+++ src/api/employees.ts
@@ -1,7 +1,6 @@
 import sortBy from 'lodash/sortBy';
 
 import { employees } from '@/lib/mocks/employees';
-import { sleep } from '@/lib/utils/async';
 
 export async function getEmployees() {
   // await sleep();
@@ -11,4 +10,16 @@ export async function getEmployees() {
 export async function getEmployee(id: string) {
   // await sleep();
   return employees.find(e => e.id === id);
+}
+
+export async function createEmployee() {
+  // return backend.post<any>('/api/employees', values);
+}
+
+export async function updateEmployee(id: string) {
+  // return backend.put<any>(`/api/employees/${id}`, values);
+}
+
+export async function deleteEmployee(id: string) {
+  // return backend.delete<any>(`/api/employees/${id}`);
 }
