b3c8bd4297e3a7920608883952271367f6388a34
Kristijan Korać <korac.kristijan@gmail.com>
2024-07-17 21:46:35 +0200

    refactor: heading

--- src/app/dashboard/employees/new/page.tsx
+++ src/app/dashboard/employees/new/page.tsx
@@ -1,4 +1,4 @@
-import { Box, Text } from '@/ui';
+import { Box, Heading } from '@/ui';
 
 import { BackButton } from './BackButton';
 import { NewEmployeeForm } from './NewEmployeeForm';
@@ -7,7 +7,7 @@ export default async function Page() {
   return (
     <Box>
       <Box className="py-5 flex flex-col gap-10">
-        <Text variant="text-xxl-medium">New Employee</Text>
+        <Heading as="h1" variant="text-xl">New Employee</Heading>
       </Box>
       <Box className="py-5 flex flex-col gap-10">
         <BackButton />
