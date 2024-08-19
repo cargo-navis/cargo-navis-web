1b63934b2e3eeb7a78d0cd182dd57d51992a7474
Kristijan Korać <korac.kristijan@gmail.com>
2024-08-19 20:46:55 +0200

    refactor(Checkbox): update UI for disabled state

--- src/lib/components/form/FormCheckboxGroup.tsx
+++ src/lib/components/form/FormCheckboxGroup.tsx
@@ -7,13 +7,19 @@ interface FormCheckboxGroupProps extends Omit<CheckboxGroupWithLabelsProps, 'val
   rules?: UseControllerProps['rules'];
 }
 
-export const FormCheckboxGroup: React.FC<FormCheckboxGroupProps> = ({ name, initialValue, rules, ...rest }) => {
+export const FormCheckboxGroup: React.FC<FormCheckboxGroupProps> = ({ name, initialValue, rules, isDisabled, ...rest }) => {
   const {
     field: { value, onChange },
     formState: { isSubmitting },
   } = useController({ name, defaultValue: initialValue, rules });
 
   return (
-    <CheckboxGroupWithLabels name={name} {...rest} isDisabled={isSubmitting} values={value || []} onChange={onChange} />
+    <CheckboxGroupWithLabels
+      name={name}
+      {...rest}
+      isDisabled={isDisabled && isSubmitting}
+      values={value || []}
+      onChange={onChange}
+    />
   );
 }
--- src/ui/components/Checkbox/Checkbox.tsx
+++ src/ui/components/Checkbox/Checkbox.tsx
@@ -14,7 +14,7 @@ export interface CheckboxProps {
 
 export const Checkbox: React.FC<CheckboxProps> = ({ isDisabled, value, label, onChange, name }) => {
   return (
-    <Field className="flex items-center gap-3">
+    <Field className={clsx("flex items-center gap-3", isDisabled && 'opacity-50')}>
       <HeadlessCheckbox
         name={name}
         disabled={isDisabled}
@@ -22,13 +22,14 @@ export const Checkbox: React.FC<CheckboxProps> = ({ isDisabled, value, label, on
         onChange={onChange}
         className={clsx(
           "group flex items-center justify-center size-[24px]",
-          "rounded-s border-[2px] border-dark-300 dark:border-light-800 hover:border-dark-500 hover:dark:border-light-700",
-          "bg-white-alpha-10 data-[checked]:bg-teal-700 dark:data-[checked]:bg-teal-600 text-white data-[checked]:!border-transparent cursor-pointer"
+          "rounded-s border-[2px] border-dark-300 dark:border-light-800",
+          "enabled:hover:border-dark-500 enabled:hover:dark:border-light-700",
+          "bg-white-alpha-10 data-[checked]:bg-teal-700 dark:data-[checked]:bg-teal-600 text-white data-[checked]:!border-transparent enabled:cursor-pointer"
         )}
       >
         <Icon color="red" icon="CheckIcon" className="opacity-0 group-data-[checked]:opacity-100 transition-opacity duration-75" size="m" />
       </HeadlessCheckbox>
-      <Label className="cursor-pointer">
+      <Label className="enabled:cursor-pointer">
         <Text color="text-color-1" variant="text-s">{label}</Text>
       </Label>
     </Field>
