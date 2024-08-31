b4f02a659321f6e231e20162398bfe8c3e8d7577
Kristijan Korać <korac.kristijan@gmail.com>
2024-08-31 15:10:32 +0200

    feat: add login screen

---
+++ src/app/login/page.tsx
@@ -0,0 +1,21 @@
+import { FlexLayout, Heading, Text } from '@/ui';
+import { LoginForm } from './loginForm';
+
+export default async function Page() {
+  //
+
+  return (
+    <FlexLayout className="h-screen flex-row">
+      <FlexLayout as="main" className="flex-col flex-grow p-5 md:p-7 justify-center items-center bg-sidebar-gradient">
+        <Heading as="h1" variant="text-xl" className="text-center -mt-10 mb-9">CargoNavis</Heading>
+        <FlexLayout className="bg-gradient-to-bl from-teal-50 to-white to-50% flex-col px-8 py-7 rounded-xl gap-7 shadow-lg">
+          <FlexLayout className="flex-col gap-2">
+            <Text as="h2" color="text-color-1" variant="text-xl-medium">Log in</Text>
+            <Text color="text-color-2" variant="text-xs">Welcome back! We are happy to have you here again.</Text>
+          </FlexLayout>
+          <LoginForm />
+        </FlexLayout>
+      </FlexLayout>
+    </FlexLayout>
+  );
+}
