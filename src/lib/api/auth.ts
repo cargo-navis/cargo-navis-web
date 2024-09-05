a2f91ddea5072af5cbea09f617561d2b6a5e0559
Kristijan Korać <korac.kristijan@gmail.com>
2024-09-05 21:16:55 +0200

    chore: clear deprecated utils

--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -1,5 +1,3 @@
-import { backend } from '@/lib/services/backendService';
-
 import { AuthResponse } from './login';
 import axios from 'axios';
 
@@ -8,18 +6,8 @@ export interface LoginArgs {
   password: string;
 }
 
-// TODO - refactor
 export async function login(values: LoginArgs) {
   const response = await axios.post<AuthResponse.RootObject>(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/token`, values);
   return response.data;
-}
-
-export async function loginWithJoso() {
-  const email = "josip.grubesa@prudentology.com";
-  const password = "oyTzFZhgfpw8iF.tehNJ";
-
-  // return backend.get('https://json-placeholder.mock.beeceptor.com/posts');
-
-  return backend.post<AuthResponse.RootObject>('/auth/token', { email, password });
 }
 
