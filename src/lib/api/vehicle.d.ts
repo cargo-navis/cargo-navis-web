53ba09461e43c0ea5b4d8966724b8d8bc23d2bc6
Kristijan Korać <korac.kristijan@gmail.com>
2024-09-15 17:54:11 +0200

    wip(Fleet): add vehicle type and mock trucks

---
+++ src/lib/api/vehicle.d.ts
@@ -0,0 +1,38 @@
+export enum VehicleEnum {
+  TRUCK = 'TRUCK',
+  TRAILER = 'TRAILER',
+  SOLO = 'SOLO',
+  VAN = 'VAN',
+}
+
+export interface Vehicle {
+  type: VehicleEnum;
+  emissionStandard: string;
+  enginePower: number;
+  tankSize: number;
+  averageFuelConsumption: number;
+  tachographExpiryDate: string;
+  fireExtinguisherCheckExpiryDate: string;
+  technicalInspectionDate: string;
+  adrExpiryDate: string;
+  dimensions: Dimensions;
+  loadCapacity: number;
+  vehicleLoadType: string;
+  equipment: string[];
+  ramp: boolean;
+  codeXlCertificateExpiryDate: string;
+  brand: string;
+  manufacturingYear: number;
+  numberOfAxles: number;
+  registration: string;
+  registrationDate: string;
+  registrationExpiryDate: string;
+  emptyWeight: number;
+}
+
+export interface Dimensions {
+  width: number;
+  height: number;
+  length: number;
+  id: string;
+}
