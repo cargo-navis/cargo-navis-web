42c9c60dbafa3fbe8eb3fdf81f40c70ef9cd6905
Kristijan Korać <korac.kristijan@gmail.com>
2024-10-10 18:52:18 +0200

    refactor(AlertMenuItem): remove deprecated items

--- src/components/AlertMenu/AlertMenuItem.tsx
+++ src/components/AlertMenu/AlertMenuItem.tsx
@@ -1,7 +1,5 @@
 import type { Alert } from '@/lib/api';
-import { alerts } from '@/lib/mocks/alerts';
 import { FlexLayout, Icon } from '@/ui';
-import type { MenuComponent } from '@/ui/components/Menu/types';
 import Link from 'next/link';
 import React from 'react';
 
@@ -11,30 +9,15 @@ interface AlertMenuItemProps {
   alert: Alert;
 }
 
-export const AlertMenuItem: React.FC<AlertMenuItemProps> = ({ alert }) => {
+export const AlertMenuItem = React.forwardRef<any, AlertMenuItemProps>(({ alert }, ref) => {
   const { targetUrl, descriptionNode } = getItemData(alert);
 
   return (
-    <Link href={targetUrl}>
-      <FlexLayout className="gap-2 px-4 py-2 hover:bg-dark-50 hover:dark:bg-light-800">
+    <Link href={targetUrl} ref={ref}>
+      <FlexLayout className="gap-2 px-4 py-2 hover:bg-dark-50 hover:dark:bg-light-800 data-[highlighted]:bg-dark-50 data-[highlighted]:dark:bg-light-800">
         <Icon icon="ExclamationTriangleIcon" color="text-red-500 dark:text-red-300" className="mt-1" />
         {descriptionNode}
       </FlexLayout>
     </Link>
   );
-};
-
-export const items: MenuComponent[] = [
-  { type: 'custom', Renderer: <AlertMenuItem alert={alerts[0]} /> },
-  { type: 'custom', Renderer: <AlertMenuItem alert={alerts[1]} /> },
-  { type: 'custom', Renderer: <AlertMenuItem alert={alerts[2]} /> },
-  { type: 'custom', Renderer: <AlertMenuItem alert={alerts[3]} /> },
-  { type: 'custom', Renderer: <AlertMenuItem alert={alerts[4]} /> },
-  { type: 'custom', Renderer: <AlertMenuItem alert={alerts[5]} /> },
-  { type: 'custom', Renderer: <AlertMenuItem alert={alerts[6]} /> },
-  { type: 'custom', Renderer: <AlertMenuItem alert={alerts[7]} /> },
-  { type: 'custom', Renderer: <AlertMenuItem alert={alerts[8]} /> },
-  { type: 'custom', Renderer: <AlertMenuItem alert={alerts[9]} /> },
-  { type: 'item', text: 'Preview', iconLeft: 'EyeIcon', onClick: () => alert('Preview') },
-  { type: 'item', text: 'Edit', iconLeft: 'PencilIcon', onClick: () => alert('Edit') },
-];
+});
