ef006ba8cda1b067e14cb6a96349e99d38203fcd
Kristijan Korać <korac.kristijan@gmail.com>
2024-10-21 17:50:34 +0200

    feat(alerts): add useAlertByVehicleType hook

--- src/lib/hooks/api/alerts.ts
+++ src/lib/hooks/api/alerts.ts
@@ -1,9 +1,23 @@
-import { type Alert, getAlerts } from '@/lib/api';
+import { type Alert, getAlerts, VehicleEnum } from '@/lib/api';
 import { useQuery } from '@tanstack/react-query';
 
-export function useAlerts<TData = Alert[]>() {
+interface UseAlertsArgs<T> {
+  select?: (data: Alert[]) => T;
+  enabled?: boolean;
+}
+
+export function useAlerts<TData = Alert[]>(args?: UseAlertsArgs<TData>) {
   return useQuery<Alert[], unknown, TData>({
     queryKey: ['alerts'],
     queryFn: getAlerts,
+    ...args,
+  });
+}
+
+export function useAlertByVehicleType(vehicleType: VehicleEnum) {
+  return useAlerts({
+    select: (alerts) => {
+      return alerts.filter(a => a.alertable.type === vehicleType);
+    },
   });
 }
