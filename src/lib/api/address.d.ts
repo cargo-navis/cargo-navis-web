5ec9d176176a673ec36b76c3ff26ffc7bd3a5ace
Kristijan Korać <korac.kristijan@gmail.com>
2025-01-06 18:42:52 +0100

    feat(api): add tenant API hook and util

---
+++ src/lib/api/address.d.ts
@@ -0,0 +1,6 @@
+export interface Address {
+  name: string;
+  postalCode: string;
+  city: string;
+  country: string;
+}
--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -3,5 +3,6 @@ export * from './alerts';
 export * from './alerts.d';
 export * from './employees';
 export * from './employees.d';
+export * from './tenant';
 export * from './vehicles';
 export * from './vehicles.d';
