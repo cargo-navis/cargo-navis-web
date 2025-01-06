f73fcc6d06c9d37ad9e281b74b71f3dce777cb66
Kristijan Korać <korac.kristijan@gmail.com>
2025-01-06 18:35:36 +0100

    feat: add UserProfilePage

--- src/components/layout/DashboardLayout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout/DashboardLayout.tsx
@@ -9,9 +9,9 @@ export const DashboardLayout: React.FC<{ children: React.ReactNode }> = ({ child
       <Box className="shrink-0 translate-x-[-16rem] flex flex-col absolute h-screen w-[256px] py-5 px-4 md:static md:h-auto md:w-64 md:translate-x-0 bg-sidebar-gradient transition ease-in-out">
         <Sidebar />
       </Box>
-      <Box as="main" className="flex-grow">
+      <Box as="main" className="flex-grow overflow-y-auto">
         <TopBar />
-        <Box className="p-5 md:p-7 overflow-y-auto">{children}</Box>
+        <Box className="p-5 md:p-7">{children}</Box>
       </Box>
     </FlexLayout>
   );
---
+++ src/pages-components/Dashboard/UserProfilePage/UserProfilePage.tsx
@@ -0,0 +1,45 @@
+import { DashboardLayout } from '@/components/layout/DashboardLayout';
+import type { User } from '@/lib/api/user.d';
+import { LoadingPage } from '@/lib/components/LoadingPage';
+import { useCurrentUser } from '@/lib/hooks';
+import { OccupationPill } from '@/pages-components/Dashboard/EmployeesPage/OccupationPill';
+import { Box, FlexLayout, Text } from '@/ui';
+import { TextInputWithLabels } from '@/ui/hocs';
+
+export const UserProfilePage = () => {
+  const { data: user, isLoading } = useCurrentUser();
+
+  return (
+    <DashboardLayout>
+      {!user || isLoading ? (
+        <LoadingPage />
+      ) : (
+        <Box>
+          <MainContent user={user} />
+        </Box>
+      )}
+    </DashboardLayout>
+  );
+};
+
+export const MainContent: React.FC<{ user: User }> = ({ user }) => {
+  return (
+    <FlexLayout className="flex-col gap-4 max-w-[400px]">
+      <FlexLayout className="gap-3 items-center">
+        <Text color="text-color-1" variant="text-xxl-bold">
+          {user.firstName} {user.lastName}
+        </Text>
+        <OccupationPill occupation={user.position} text={user.position} />
+      </FlexLayout>
+      <Box>
+        <TextInputWithLabels
+          label="Email"
+          isDisabled
+          value={user.email}
+          iconLeft="LockClosedIcon"
+          onChange={() => null}
+        />
+      </Box>
+    </FlexLayout>
+  );
+};
