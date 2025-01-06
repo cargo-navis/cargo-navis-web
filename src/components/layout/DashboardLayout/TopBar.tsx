3d78927795c0e8a64a6e44d12e35050dc12283a6
Kristijan Korać <korac.kristijan@gmail.com>
2025-01-06 19:06:42 +0100

    feat(Sidebar): add tenant link

--- src/components/layout/DashboardLayout/TopBar.tsx
+++ src/components/layout/DashboardLayout/TopBar.tsx
@@ -9,10 +9,7 @@ export const TopBar = () => {
     <FlexLayout className="justify-end w-full p-2 border-0 border-b-[1px] border-b-black-alpha-10 dark:border-b-white-alpha-10">
       <DisplayIf condition={!!user && !isLoading} fallback={<LoadingSpinner />}>
         <Link href="/dashboard/profile">
-          <FlexLayout
-            as="a"
-            className="bg-black-alpha-05 dark:bg-white-alpha-10 hover:bg-black-alpha-10 dark:hover:bg-white-alpha-25 rounded-l justify-center items-center px-3 py-2"
-          >
+          <FlexLayout className="bg-black-alpha-05 dark:bg-white-alpha-10 hover:bg-black-alpha-10 dark:hover:bg-white-alpha-25 rounded-l justify-center items-center px-3 py-2">
             <Text color="text-color-2" variant="text-m-medium">
               {user?.firstName} {user?.lastName[0]}.
             </Text>
