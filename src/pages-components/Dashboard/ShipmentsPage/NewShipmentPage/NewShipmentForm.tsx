e13c2021e814361457a762126f054da18440836f
Kristijan Korać <korac.kristijan@gmail.com>
2025-01-11 22:48:43 +0100

    wip(Shipments): add initial shipments form

---
+++ src/pages-components/Dashboard/ShipmentsPage/NewShipmentPage/NewShipmentForm.tsx
@@ -0,0 +1,117 @@
+import { FormSingleSelect, FormTextInput } from '@/lib/components/form';
+import { FormTextarea } from '@/lib/components/form/FormTextarea';
+import { Box, Divider, FlexLayout, Text, TextButton } from '@/ui';
+import { FormProvider, useForm } from 'react-hook-form';
+
+const defaultValues = {
+  orderNo: '2025/24',
+};
+
+export const NewShipmentForm = () => {
+  const formMethods = useForm<any>({
+    defaultValues,
+    mode: 'all',
+  });
+
+  const { handleSubmit } = formMethods;
+
+  function handleFormSubmit(data: any) {
+    console.log(data);
+  }
+
+  return (
+    <FormProvider {...formMethods}>
+      <FlexLayout as="form" className="flex-col gap-[40px]" onSubmit={handleSubmit(handleFormSubmit)}>
+        <FlexLayout className="flex-col gap-4 w-[520px]">
+          <FlexLayout as="fieldset" className="flex-col gap-5">
+            <FlexLayout className="gap-4">
+              <Box className="flex-1">
+                <FormTextInput iconLeft="LockClosedIcon" name="orderNo" label="Broj naloga" isDisabled />
+              </Box>
+              <Box className="flex-1">
+                <FormTextInput name="referenceNumber" label="Referentni broj" placeholder="1234" />
+              </Box>
+            </FlexLayout>
+            <FlexLayout className="gap-4">
+              <Box className="flex-1">
+                <FormSingleSelect
+                  label="Klijent"
+                  name="client"
+                  isSearchable
+                  options={[]}
+                  placeholder="Odaberi klijenta..."
+                />
+              </Box>
+              <Box className="flex-1">
+                <FormTextInput
+                  iconLeft="CurrencyEuroIcon"
+                  name="price"
+                  label="Cijena (Euro)"
+                  placeholder="XXX"
+                  type="number"
+                  min="0"
+                />
+              </Box>
+            </FlexLayout>
+          </FlexLayout>
+          <Box className="py-4">
+            <Divider />
+          </Box>
+          <CargoFields />
+          <Box className="py-4">
+            <Divider />
+          </Box>
+          <AddressFields />
+          <TextButton iconLeft="PlusIcon" text="Dodaj podnalog" variant="secondary" />
+        </FlexLayout>
+      </FlexLayout>
+    </FormProvider>
+  );
+};
+
+const CargoFields = () => {
+  return (
+    <FlexLayout as="fieldset" className="flex-col gap-4">
+      <Text color="text-color-3" variant="text-s-medium">
+        TERET
+      </Text>
+      <FormTextInput name="ldm" placeholder="1.00" label="LDM" type="number" step="0.01" />
+      {/* TODO - add dimensions */}
+      <FormTextInput name="wight" placeholder="100" label="Težina (kg)" />
+      <FormTextarea name="description" label="Opis tereta" />
+    </FlexLayout>
+  );
+};
+
+const AddressFields = () => {
+  return (
+    <FlexLayout as="fieldset" className="gap-4">
+      <FlexLayout className="flex-1 flex-col gap-2">
+        <Text color="text-color-3" variant="text-xxs-medium">
+          Adresa utovara
+        </Text>
+        <FlexLayout className="flex-col gap-5">
+          <Box className="flex-1">
+            <FormTextInput name="streetName" placeholder="Ulica" />
+          </Box>
+          <Box className="flex-1">
+            <FormSingleSelect name="postalCode1" isSearchable placeholder="Poštanski broj" options={[]} />
+          </Box>
+        </FlexLayout>
+      </FlexLayout>
+      <FlexLayout className="flex-1 flex-col gap-2">
+        <Text color="text-color-3" variant="text-xxs-medium">
+          Adresa istovara
+        </Text>
+        <FlexLayout className="flex-col gap-5">
+          <Box className="flex-1">
+            <FormTextInput name="streetName" placeholder="Ime ulice" />
+          </Box>
+          <Box className="flex-1">
+            <FormSingleSelect name="postalCode2" isSearchable placeholder="Poštanski broj" options={[]} />
+          </Box>
+        </FlexLayout>
+      </FlexLayout>
+    </FlexLayout>
+  );
+};
--- src/pages-components/Dashboard/ShipmentsPage/NewShipmentPage/NewShipmentPage.tsx
+++ src/pages-components/Dashboard/ShipmentsPage/NewShipmentPage/NewShipmentPage.tsx
@@ -1,6 +1,7 @@
 import { DashboardLayout } from '@/components/layout/DashboardLayout';
 import { BackButton } from '@/pages-components/Dashboard/NewEmployeePage/BackButton';
 import { Box, FlexLayout, Heading } from '@/ui';
+import { NewShipmentForm } from './NewShipmentForm';
 
 export const NewShipmentPage = () => {
   return (
@@ -13,7 +14,7 @@ export const NewShipmentPage = () => {
         </FlexLayout>
         <FlexLayout className="py-5 flex-col gap-[40px]">
           <BackButton targetLocation="/dashboard/shipments" />
-          {/*  NewShipmentForm */}
+          <NewShipmentForm />
         </FlexLayout>
       </Box>
     </DashboardLayout>
