72a39ec66b01b4e736ff8093ab7945251b9713f8
Kristijan Korać <korac.kristijan@gmail.com>
2025-02-07 18:54:22 +0700

    chore(clients): add mock data

---
+++ src/lib/mocks/clients.ts
@@ -0,0 +1,124 @@
+import type { Client } from '@/lib/api';
+
+export const clients: Client[] = [
+  {
+    id: '1a2b3c4d-1234-5678-9abc-def012345678',
+    name: 'COMBIS d.o.o.',
+    address: {
+      name: 'Ilica 10',
+      postalCode: '10000',
+      city: 'Zagreb',
+      country: 'Croatia',
+    },
+    vatNumber: 'HR12345678901',
+    nationalCompanyRegisterId: '080123456',
+  },
+  {
+    id: '2b3c4d5e-2345-6789-0bcd-ef1234567890',
+    name: 'Zagrebački holding d.o.o.',
+    address: {
+      name: 'Vukovarska ulica 45',
+      postalCode: '10000',
+      city: 'Zagreb',
+      country: 'Croatia',
+    },
+    vatNumber: 'HR23456789012',
+    nationalCompanyRegisterId: '080234567',
+  },
+  {
+    id: '3c4d5e6f-3456-7890-1cde-f01234567890',
+    name: 'Hrvatska pošta d.d.',
+    address: {
+      name: 'Jelačićev trg 3',
+      postalCode: '10000',
+      city: 'Zagreb',
+      country: 'Croatia',
+    },
+    vatNumber: 'HR34567890123',
+    nationalCompanyRegisterId: '080345678',
+  },
+  {
+    id: '4d5e6f7g-4567-8901-2def-012345678901',
+    name: 'Podravka d.d.',
+    address: {
+      name: 'Korzo 22',
+      postalCode: '48000',
+      city: 'Koprivnica',
+      country: 'Croatia',
+    },
+    vatNumber: 'HR45678901234',
+    nationalCompanyRegisterId: '080456789',
+  },
+  {
+    id: '5e6f7g8h-5678-9012-3ef0-123456789012',
+    name: 'Atlantic Grupa d.d.',
+    address: {
+      name: 'Poljička cesta 150',
+      postalCode: '10000',
+      city: 'Zagreb',
+      country: 'Croatia',
+    },
+    vatNumber: 'HR56789012345',
+    nationalCompanyRegisterId: '080567890',
+  },
+  {
+    id: '6f7g8h9i-6789-0123-4f01-234567890123',
+    name: 'KONČAR - Elektroindustrija d.d.',
+    address: {
+      name: 'Radnička cesta 80',
+      postalCode: '10000',
+      city: 'Zagreb',
+      country: 'Croatia',
+    },
+    vatNumber: 'HR67890123456',
+    nationalCompanyRegisterId: '080678901',
+  },
+  {
+    id: '7g8h9i0j-7890-1234-5g12-345678901234',
+    name: 'INA - Industrija nafte d.d.',
+    address: {
+      name: 'Frankopanska ulica 12',
+      postalCode: '10000',
+      city: 'Zagreb',
+      country: 'Croatia',
+    },
+    vatNumber: 'HR78901234567',
+    nationalCompanyRegisterId: '080789012',
+  },
+  {
+    id: '8h9i0j1k-8901-2345-6h23-456789012345',
+    name: 'Ericsson Nikola Tesla d.d.',
+    address: {
+      name: 'Ante Starčevića 27',
+      postalCode: '10000',
+      city: 'Zagreb',
+      country: 'Croatia',
+    },
+    vatNumber: 'HR89012345678',
+    nationalCompanyRegisterId: '080890123',
+  },
+  {
+    id: '9i0j1k2l-9012-3456-7i34-567890123456',
+    name: 'Adris grupa d.d.',
+    address: {
+      name: 'Ulica kralja Zvonimira 8',
+      postalCode: '52210',
+      city: 'Rovinj',
+      country: 'Croatia',
+    },
+    vatNumber: 'HR90123456789',
+    nationalCompanyRegisterId: '080901234',
+  },
+  {
+    id: '0j1k2l3m-0123-4567-8j45-678901234567',
+    name: 'Agrokor d.d.',
+    address: {
+      name: 'Teslina ulica 5',
+      postalCode: '10000',
+      city: 'Zagreb',
+      country: 'Croatia',
+    },
+    vatNumber: 'HR01234567890',
+    nationalCompanyRegisterId: '080012345',
+  },
+];
