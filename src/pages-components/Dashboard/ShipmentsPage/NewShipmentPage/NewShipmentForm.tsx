e4c0648c844a6a8bb056175170880d1ebfad013a
Kristijan Korać <korac.kristijan@gmail.com>
2025-03-15 14:04:52 +0100

    feat(NewShipmentForm): refactor and add edit functionality

--- src/pages-components/Dashboard/ShipmentsPage/NewShipmentPage/NewShipmentForm.tsx
+++ src/pages-components/Dashboard/ShipmentsPage/NewShipmentPage/NewShipmentForm.tsx
@@ -1,66 +1,62 @@
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useRouter } from 'next/router';
-import { FormProvider, useForm, useFormContext } from 'react-hook-form';
+import { FormProvider, useForm } from 'react-hook-form';
 
+import type { Shipment } from '@/lib/api';
 import { FormTextInput } from '@/lib/components/form';
-import { useCreateShipment } from '@/lib/hooks';
-import { Box, Button, Divider, FlexLayout, TextButton } from '@/ui';
+import { useCreateShipment, useUpdateShipment } from '@/lib/hooks';
+import { Box, Button, Divider, FlexLayout, LoadingSpinner } from '@/ui';
 
 import AddressFields from './AddressFields';
-import { CargoField } from './CargoField';
+import { CargoFieldList } from './CargoFieldList';
 import { ClientField } from './ClientField';
 import { ContractorField } from './ContractorField';
 import { DispatcherField } from './DispatcherField';
 import { DriverField } from './DriverField';
 import { shipmentSchema } from './schema';
-import { Cargo, ShipmentFields } from './types';
-import { PalleteType, transformFormDataToPayload } from './utils';
+import type { ShipmentFields } from './types';
+import { getFormDefaultValues, transformFormDataToPayload } from './utils';
 import { VehicleField } from './VehicleField';
 
-const defaultCargo: Cargo = {
-  weight: 0,
-  description: '',
-  ldm: 0.4,
-  metadata: {
-    type: 'standard',
-    palleteType: PalleteType.Euro,
-    palleteAmount: 1,
-  },
-};
+interface NewShipmentFormProps {
+  shipment?: Shipment;
+}
 
-const defaultValues: ShipmentFields = {
-  orderNumber: '2025/24',
-  cargo: [defaultCargo],
-};
-
-export const NewShipmentForm = () => {
+export const NewShipmentForm: React.FC<NewShipmentFormProps> = ({ shipment }) => {
   const { push } = useRouter();
+  const isEdit = !!shipment;
+
   const { mutateAsync: createShipment } = useCreateShipment();
+  const { mutateAsync: updateShipment } = useUpdateShipment();
 
   const formMethods = useForm<ShipmentFields>({
-    defaultValues,
-    mode: 'all',
+    defaultValues: getFormDefaultValues(shipment),
     resolver: yupResolver(shipmentSchema),
+    mode: 'all',
   });
 
   const { handleSubmit, formState } = formMethods;
-  const { isValid, isSubmitting } = formState;
+  const { isDirty, isValid, isLoading, isSubmitting } = formState;
 
   async function handleFormSubmit(data: ShipmentFields) {
     const payload = transformFormDataToPayload(data);
     console.log(payload);
 
     try {
-      // if (isEdit) {
-      //   await updateClient(payload);
-      //   await push(`/dashboard/clients/${client.id}`);
-      // } else {
-      await createShipment(payload);
-      await push('/dashboard/shipments');
-      // }
+      if (isEdit && shipment) {
+        await updateShipment({ id: shipment.id, ...payload });
+        await push(`/dashboard/shipments/${shipment.id}`);
+      } else {
+        await createShipment(payload);
+        await push('/dashboard/shipments');
+      }
     } catch {
       alert('Dogodila se greška s unosom naloga. Pokušajte ponovno.');
     }
+  }
+
+  if (isLoading) {
+    return <LoadingSpinner size="l" />;
   }
 
   return (
@@ -117,51 +113,16 @@ export const NewShipmentForm = () => {
           </FlexLayout>
           <Box className="sticky bottom-0 bg-[#e9eded] dark:bg-black border-t-[2px] border-dark-200 dark:border-light-700 p-4 -mx-4">
             <Button
-              isDisabled={!isValid}
+              isDisabled={!(isValid && isDirty)}
               isFullWidth
               isLoading={isSubmitting}
-              text="Napravi nalog"
+              text={isEdit ? 'Ažuriraj nalog' : 'Napravi nalog'}
               type="submit"
               variant="primary"
             />
           </Box>
         </FlexLayout>
-        {/* <ValuesPrinter /> */}
       </Box>
     </FormProvider>
   );
 };
-
-const CargoFieldList = () => {
-  const { watch, setValue } = useFormContext<ShipmentFields>();
-  const cargo = watch('cargo');
-
-  return (
-    <FlexLayout className="flex-1 flex-col gap-4">
-      {cargo.map((_, index: number, arr: Cargo[]) => (
-        <CargoField cargoLength={arr.length} index={index} key={index} />
-      ))}
-      <TextButton
-        iconLeft="PlusIcon"
-        text="Dodaj teret"
-        type="button"
-        variant="secondary"
-        onClick={() => setValue('cargo', [...cargo, defaultCargo])}
-      />
-    </FlexLayout>
-  );
-};
-
-// const ValuesPrinter = () => {
-//   const { watch } = useFormContext();
-//   const formValues = watch();
-
-//   return (
-//     <Box className="fixed right-4 top-4 w-[400px] p-4 rounded-lg shadow-lg overflow-auto max-h-[90vh]">
-//       <Text className="mb-2" variant="text-s-medium">
-//         Form Data:
-//       </Text>
-//       <pre className="text-xs whitespace-pre-wrap">{JSON.stringify(formValues, null, 2)}</pre>
-//     </Box>
-//   );
-// };
