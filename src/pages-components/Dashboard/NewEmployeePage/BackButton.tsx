30d61874ce08f32c4c1a4fabdf0fd77b4bf24904
Kristijan Korać <korac.kristijan@gmail.com>
2025-03-20 18:58:24 +0100

    fix(BackButton): fix logic for navigation

--- src/pages-components/Dashboard/NewEmployeePage/BackButton.tsx
+++ src/pages-components/Dashboard/NewEmployeePage/BackButton.tsx
@@ -5,8 +5,12 @@ import { useRouter } from 'next/navigation';
 
 import { FlexLayout, Icon, Text } from '@/ui';
 
+const canGoBackInHistory = (): boolean => {
+  return typeof window !== 'undefined' && 'navigation' in window && !!(window as any).navigation?.canGoBack;
+};
+
 export function BackButton({ targetLocation }: { targetLocation: string }) {
-  const { push } = useRouter();
+  const { push, back } = useRouter();
 
   return (
     <FlexLayout
@@ -14,7 +18,7 @@ export function BackButton({ targetLocation }: { targetLocation: string }) {
         'items-center gap-1 cursor-pointer max-w-max',
         'text-color-2 hover:text-teal-500 transition-colors duration-75'
       )}
-      onClick={() => push(targetLocation)}
+      onClick={() => (canGoBackInHistory() ? back() : push(targetLocation))}
     >
       <Icon icon="ArrowUturnLeftIcon" />
       <Text variant="text-s-medium">Natrag</Text>
