fc364a9bb1b766e52e3d4414900170165ae4c0ec
Kristijan Korać <korac.kristijan@gmail.com>
2025-04-12 20:00:55 +0200

    fix(ShipmentTable): remove warning for non-tenant transporters

--- src/pages-components/Dashboard/ShipmentsPage/ShipmentsTable.tsx
+++ src/pages-components/Dashboard/ShipmentsPage/ShipmentsTable.tsx
@@ -208,11 +208,12 @@ export function ShipmentsTable({ shipments }: { shipments?: Shipment[] }) {
           const displayValue = vehicle ? `${vehicle?.registration} (${vehicle?.brand})` : '—';
 
           const isSubshipment = info.row.depth !== 0;
+          const isTenantTransporter = info.row.original.transportContractorId === tenant?.id;
 
           return (
             <FlexLayout className="items-center py-2 group-hover/row:text-teal-500 gap-2">
               <Text>{displayValue}</Text>
-              <DisplayIf condition={!isSubshipment && !vehicleId}>
+              <DisplayIf condition={!isSubshipment && !vehicleId && isTenantTransporter}>
                 <Tooltip
                   content={
                     <Box className="px-1">
@@ -238,11 +239,11 @@ export function ShipmentsTable({ shipments }: { shipments?: Shipment[] }) {
           const fullName = employee ? `${employee.firstName || ''} ${employee.lastName || ''}`.trim() : '—';
 
           const isSubshipment = info.row.depth !== 0;
-
+          const isTenantTransporter = info.row.original.transportContractorId === tenant?.id;
           return (
             <FlexLayout className="items-center py-2 group-hover/row:text-teal-500 gap-2">
               <Text>{fullName || '—'}</Text>
-              <DisplayIf condition={!isSubshipment && !driverId}>
+              <DisplayIf condition={!isSubshipment && !driverId && isTenantTransporter}>
                 <Tooltip
                   content={
                     <Box className="px-1">
