4377ee4f44f5c7550154c9ff9814a027ca8619d4
Kristijan Korać <korac.kristijan@gmail.com>
2025-06-27 21:14:26 +0200

    feat(InvoiceStatus): add new Paid status option

--- src/pages-components/Dashboard/ShipmentsPage/SingleShipmentPage/components/InvoiceItem.tsx
+++ src/pages-components/Dashboard/ShipmentsPage/SingleShipmentPage/components/InvoiceItem.tsx
@@ -12,21 +12,23 @@ interface InvoiceItemProps {
 
 export const InvoiceItem: React.FC<InvoiceItemProps> = ({ invoiceStatus, onChange, isPending }) => {
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+  const isInvoiceNotSent = invoiceStatus === InvoiceStatus.NotSent;
   const isInvoiceSent = invoiceStatus === InvoiceStatus.Sent;
+  const isInvoicePaid = invoiceStatus === InvoiceStatus.Paid;
 
   return (
     <FlexLayout className="items-center">
       <Box
         className={clsx(
           'cursor-pointer transition-opacity',
-          !isInvoiceSent || hoveredIndex === 0 ? 'opacity-100' : 'opacity-50'
+          isInvoiceNotSent || hoveredIndex === 0 ? 'opacity-100' : 'opacity-50'
         )}
         isDisabled={isPending}
         onClick={() => !isPending && onChange(InvoiceStatus.NotSent)}
         onMouseEnter={() => !isPending && setHoveredIndex(0)}
         onMouseLeave={() => !isPending && setHoveredIndex(null)}
       >
-        <Pill text="Nefakturiran" variant={!isInvoiceSent || hoveredIndex === 0 ? 'danger' : 'default'} />
+        <Pill text="Nefakturiran" variant={isInvoiceNotSent || hoveredIndex === 0 ? 'danger' : 'default'} />
       </Box>
       <Box className="w-5 h-[2px] bg-light-200 dark:bg-white-alpha-25 mx-2" />
       <Box
@@ -39,7 +41,20 @@ export const InvoiceItem: React.FC<InvoiceItemProps> = ({ invoiceStatus, onChang
         onMouseEnter={() => !isPending && setHoveredIndex(1)}
         onMouseLeave={() => !isPending && setHoveredIndex(null)}
       >
-        <Pill text="Fakturiran" variant={isInvoiceSent || hoveredIndex === 1 ? 'success' : 'default'} />
+        <Pill text="Fakturiran" variant={isInvoiceSent || hoveredIndex === 1 ? 'info' : 'default'} />
+      </Box>
+      <Box className="w-5 h-[2px] bg-light-200 dark:bg-white-alpha-25 mx-2" />
+      <Box
+        className={clsx(
+          'cursor-pointer transition-opacity',
+          isInvoicePaid || hoveredIndex === 2 ? 'opacity-100' : 'opacity-50'
+        )}
+        isDisabled={isPending}
+        onClick={() => !isPending && onChange(InvoiceStatus.Paid)}
+        onMouseEnter={() => !isPending && setHoveredIndex(2)}
+        onMouseLeave={() => !isPending && setHoveredIndex(null)}
+      >
+        <Pill text="Plaćen 💰" variant={isInvoicePaid || hoveredIndex === 2 ? 'success' : 'default'} />
       </Box>
     </FlexLayout>
   );
