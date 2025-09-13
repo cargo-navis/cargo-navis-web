30f8dcdb733710d35adf08eaec7350b2949e1373
Kristijan Korać <korac.kristijan@gmail.com>
2025-09-13 19:43:44 +0200

    feat(ShipmentForm): display checkbox with respect to driver messageChannel

--- src/pages-components/Dashboard/ShipmentsPage/NewShipmentPage/DriverField.tsx
+++ src/pages-components/Dashboard/ShipmentsPage/NewShipmentPage/DriverField.tsx
@@ -1,7 +1,9 @@
+import { useFormContext } from 'react-hook-form';
+
 import { FormCheckbox, FormSingleSelect } from '@/lib/components/form';
 import { useDrivers } from '@/lib/hooks';
 import { mapEmployeesToOptions } from '@/lib/utils/employees';
-import { Box, FlexLayout } from '@/ui';
+import { Box, DisplayIf, FlexLayout } from '@/ui';
 
 import { useAgencyFieldReset } from './hooks';
 
@@ -10,6 +12,12 @@ export const DriverField = () => {
   const driverOptions = mapEmployeesToOptions(drivers);
 
   const isAgencyUse = useAgencyFieldReset('driverId');
+
+  const { watch } = useFormContext();
+  const driverId = watch('driverId');
+
+  const selectedDriver = drivers.find((driver) => driver.id === driverId);
+  const isCheckboxDisplayed = !!selectedDriver?.messageChannel;
 
   // Hide the component completely when isAgencyUse is true
   if (isAgencyUse) {
@@ -26,9 +34,11 @@ export const DriverField = () => {
         options={driverOptions}
         placeholder="Odaberi vozača..."
       />
-      <Box className="-mt-2">
-        <FormCheckbox label="Automatski poslati nalog vozaču" name="sentToDriver" />
-      </Box>
+      <DisplayIf condition={isCheckboxDisplayed}>
+        <Box className="-mt-2">
+          <FormCheckbox label="Automatski poslati nalog vozaču" name="sentToDriver" />
+        </Box>
+      </DisplayIf>
     </FlexLayout>
   );
 };
