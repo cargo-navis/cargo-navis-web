e4e8754c5b44c18a8140f96102dfaaaa97d95dd4
Kristijan Korać <korac.kristijan@gmail.com>
2025-09-24 19:48:31 +0200

    ui(Switch): make form component

--- src/lib/components/form/index.ts
+++ src/lib/components/form/index.ts
@@ -4,6 +4,7 @@ export * from './FormCheckboxGroup';
 export * from './FormDatepicker';
 export * from './FormRadioGroup';
 export * from './FormSingleSelect';
+export * from './FormSwitch';
 export * from './FormTextarea';
 export * from './FormTextInput';
 export * from './FormYearpicker';
