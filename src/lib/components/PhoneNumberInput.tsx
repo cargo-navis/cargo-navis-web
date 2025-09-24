0f2ee295b633042a7a0e0e6a8fed65ef89cdc9f2
Kristijan Korać <korac.kristijan@gmail.com>
2025-09-24 21:00:17 +0200

    feat: add new PhoneNumberInput component

---
+++ src/lib/components/PhoneNumberInput.tsx
@@ -0,0 +1,71 @@
+import clsx from 'clsx';
+
+import { Box, FlexLayout, VerticalDivider } from '@/ui';
+import { SingleSelect } from '@/ui/components/Select/SingleSelect/SingleSelect';
+import { TextInput } from '@/ui/components/TextInput/TextInput';
+
+export type PhoneNumberValue = {
+  countryCode: string;
+  phoneNumber: string;
+};
+
+interface PhoneNumberInputProps {
+  placeholder?: string;
+  isDisabled?: boolean;
+  value: PhoneNumberValue;
+  onChange(value: PhoneNumberValue): void;
+}
+
+const countryOptions = [
+  { value: '+385', label: '🇭🇷 +385' },
+  { value: '+386', label: '🇸🇮 +386' },
+  { value: '+387', label: '🇧🇦 +387' },
+];
+
+export const PhoneNumberInput: React.FC<PhoneNumberInputProps> = ({
+  value,
+  onChange,
+  placeholder = 'Unesi broj telefona',
+  isDisabled = false,
+}) => {
+  function handleCountryCodeChange(newCountryCode: string | number) {
+    const newValue = { ...value, countryCode: newCountryCode.toString() };
+    onChange(newValue);
+  }
+
+  function handlePhoneNumberChange(newPhoneNumber: string) {
+    const newValue = { ...value, phoneNumber: newPhoneNumber };
+    onChange(newValue);
+  }
+
+  return (
+    <FlexLayout
+      className={clsx(
+        'items-center py-1 gap-2 [&_*]:!border-none',
+        'border-[2px] rounded-s border-dark-300 dark:border-light-800',
+        'hover:border-dark-500 hover:dark:border-light-700',
+        'focus-within:!border-teal-600 dark:focus-within:!border-teal-800'
+      )}
+    >
+      <Box className="flex-shrink-0">
+        <SingleSelect
+          isDisabled={isDisabled}
+          options={countryOptions}
+          placeholder="Odaberi državu"
+          value={value.countryCode}
+          onChange={handleCountryCodeChange}
+        />
+      </Box>
+      <VerticalDivider />
+      <Box className="flex-grow">
+        <TextInput
+          isDisabled={isDisabled}
+          placeholder={placeholder}
+          type="tel"
+          value={value.phoneNumber}
+          onChange={handlePhoneNumberChange}
+        />
+      </Box>
+    </FlexLayout>
+  );
+};
