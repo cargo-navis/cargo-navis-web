f65f8cc7c2ab9e9592b93afc4e2ccf913e16da8c
Kristijan Korać <korac.kristijan@gmail.com>
2025-11-10 19:19:04 +0100

    feat(cargo): add optimistic update on cargo update

--- src/lib/hooks/api/cargo.ts
+++ src/lib/hooks/api/cargo.ts
@@ -1,6 +1,6 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
-import { updateCargo } from '@/lib/api';
+import { Shipment, updateCargo } from '@/lib/api';
 
 export function useUpdateCargo(shipmentId: string) {
   const queryClient = useQueryClient();
@@ -11,24 +11,22 @@ export function useUpdateCargo(shipmentId: string) {
       return updateCargo(shipmentId, id, cargoData);
     },
     onMutate: (data) => {
-      // const prevShipment = queryClient.getQueryData<Shipment>(['shipment', data.id]);
-      //
-      // queryClient.setQueryData(['shipment', data.id], (oldData: Shipment | undefined) => {
-      //   if (!oldData) return oldData;
-      //   return { ...oldData, ...data };
-      // });
-      //
-      // return { prevShipment };
+      const prevShipment = queryClient.getQueryData<Shipment>(['shipment', shipmentId]);
+
+      queryClient.setQueryData(['shipment', data.id], (shipment: Shipment | undefined) => {
+        if (!shipment) return shipment;
+        return { ...shipment, cargo: { ...shipment.cargo, ...data } };
+      });
+
+      return { prevShipment };
     },
-    onSuccess: (_, variables) => {
-      // queryClient.invalidateQueries({ queryKey: ['shipment', variables.id] });
-      // queryClient.invalidateQueries({ queryKey: ['shipments'] });
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['shipment', shipmentId] });
+      queryClient.invalidateQueries({ queryKey: ['shipments'] });
     },
-    onError: (_, data, context) => {
-      // if (!context) return;
-      // const { prevShipment } = context;
-      //
-      // queryClient.setQueryData(['shipment', data.id], prevShipment);
+    onError: (_, _data, context) => {
+      if (!context) return;
+      queryClient.setQueryData(['shipment', shipmentId], context.prevShipment);
     },
   });
 }
