1f39d8d58e1a583f014bae61cc6e3fa75ad630bf
Kristijan Korać <korac.kristijan@gmail.com>
2025-11-17 21:35:19 +0100

    refactor(ShipmentForm): update minor ui

--- src/pages-components/Dashboard/ShipmentsPage/NewShipmentPage/AgencyField.tsx
+++ src/pages-components/Dashboard/ShipmentsPage/NewShipmentPage/AgencyField.tsx
@@ -1,10 +1,5 @@
 import { FormCheckbox } from '@/lib/components/form';
-import { Box } from '@/ui';
 
 export const AgencyField: React.FC = () => {
-  return (
-    <Box className="flex-1">
-      <FormCheckbox label="Agencijski nalog" name="isAgencyUse" />
-    </Box>
-  );
+  return <FormCheckbox label="Agencijski nalog" name="isAgencyUse" />;
 };
