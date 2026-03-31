5ae590711f156359634f9d67def4ffb48f0995c5
Kristijan Korać <korac.kristijan@gmail.com>
2026-03-31 11:24:59 +0200

    ui(Pill): add border to variants

--- src/ui/components/Pill/const.ts
+++ src/ui/components/Pill/const.ts
@@ -2,10 +2,12 @@ import type { TextVariant } from '@/ui/theme/fontSizes';
 
 export const variantsMap = {
   default: 'bg-dark-50 dark:bg-light-800 text-dark-800 dark:text-light-100',
-  success: 'bg-green-100 dark:bg-green-900 text-green-800 dark:text-green-100',
-  warning: 'bg-yellow-100 dark:bg-yellow-400 text-yellow-800 dark:text-yellow-900',
-  danger: 'bg-red-100 dark:bg-red-800 text-red-800 dark:text-red-100',
-  info: 'bg-blue-100 dark:bg-blue-700 text-blue-500 dark:text-blue-100',
+  success:
+    'bg-green-100 dark:bg-green-900 border-green-500/20 dark:border-green-700 text-green-800 dark:text-green-100',
+  warning:
+    'bg-yellow-100 dark:bg-yellow-400/60 border-yellow-700/25 dark:border-yellow-500 text-yellow-800 dark:text-yellow-300',
+  danger: 'bg-red-100 dark:bg-red-800 border-red-800/20 dark:border-red-700 text-red-800 dark:text-red-100',
+  info: 'bg-blue-100 dark:bg-blue-700 border-blue-500/20 dark:border-blue-500 text-blue-500 dark:text-blue-100',
 };
 
 export type PillVariant = keyof typeof variantsMap;
